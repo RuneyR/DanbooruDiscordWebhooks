cc232418d202fcc903094dfd7a7f23a0cfc99db3
RuneyR <gamerthe11@gmail.com>
2023-06-21 18:05:48 -0700

    Apparently, file url doesn't exist sometimes. So, assuming large file url takes place, and even if it has nothing, we have some detours to handle it while keeping the code working.

--- QueryDanbooru.py
+++ QueryDanbooru.py
@@ -1,4 +1,5 @@
 import time
+import logging
 
 
 def start_loop_query(tags_webhooks: dict, DANBOORU_USERNAME: str, API_KEY: str, Timings: list):
@@ -6,6 +7,7 @@ def start_loop_query(tags_webhooks: dict, DANBOORU_USERNAME: str, API_KEY: str, 
     TIME_BETWEEN_WEBHOOK_POSTS = 1
     TEST_TIME = 0
     POSTS_PER_QUERY = Timings[3]
+    logger = logging.getLogger()
 
     from pybooru import Danbooru
     from discord_webhook import DiscordWebhook
@@ -21,12 +23,21 @@ def start_loop_query(tags_webhooks: dict, DANBOORU_USERNAME: str, API_KEY: str, 
                 for posts in postList:
                     # Finally, post the proper tags to the proper Discord Webhooks.
                     for webhooksSet in tags_webhooks.get(tags):
-                        webhook = DiscordWebhook(url=webhooksSet, content=posts['file_url'])
-                        webhook.execute()
-                        time.sleep(TIME_BETWEEN_WEBHOOK_POSTS)
+                        if "file_url" in posts:
+                            webhook = DiscordWebhook(url=webhooksSet, content=posts['file_url'])
+                            webhook.execute()
+                            time.sleep(TIME_BETWEEN_WEBHOOK_POSTS)
+                        elif "large_file_url" in posts:
+                            webhook = DiscordWebhook(url=webhooksSet, content=posts['large_file_url'])
+                            webhook.execute()
+                            time.sleep(TIME_BETWEEN_WEBHOOK_POSTS)
+                        else:
+                            print("We got problem, look below.")
+                            print(posts)
                     time.sleep(TIME_BETWEEN_POSTS)
             time.sleep(TEST_TIME)
             print("We are cycling now!")
         except Exception as e:
+            logger.exception("We have an issue here!: " + str(e))
             print(e)
             continue
